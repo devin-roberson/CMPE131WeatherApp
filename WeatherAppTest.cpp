ad3cae3a8d9588eda7e4a49ad68449772f739eed
cho-sua <joshuarieta04@gmail.com>
2025-05-18 21:49:16 -0700

    Add files via upload

--- WeatherAppTest.cpp
+++ WeatherAppTest.cpp
@@ -1,29 +1,30 @@
-#include "pch.h"
-#include "CppUnitTest.h"
-#include "weather.h"
-#include <fstream>
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+	#include "pch.h"
+	#include "CppUnitTest.h"
+	#include "weather.h"
+	#include <fstream>
+	#include <CppUnitTest.h>
+	using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
-namespace WeatherAppTest
-{
-	TEST_CLASS(WeatherAppTest)
+	namespace WeatherAppTest
 	{
-	public:
-		
-		TEST_METHOD(TestGetData)
+		TEST_CLASS(WeatherAppTest)
 		{
-			weatherStation test;
-			test.getData();
-			test.storeData();
+		public:
+		
+			TEST_METHOD(TestGetData)
+			{
+				weatherStation t;
+				Assert::IsTrue(t.getData(), L"Error gathering data.");
+				Assert::IsTrue(t.storeData(), L"Error storing data.");
 
-			std::ifstream iFile("save.data");
-			Assert::IsTrue(iFile.is_open(), L"Error opening file.");
+				std::ifstream iFile("save.data");
+				Assert::IsTrue(iFile.is_open(), L"Error opening file.");
 
-			std::string l;
-			std::getline(iFile, l);
-			iFile.close();
+				std::string l;
+				std::getline(iFile, l);
+				iFile.close();
 
-			Assert::AreEqual(std::string("Temp:70,Rain:1,40,Thunder:1,Hail:0,Windy:1,20"), l, L"Data does not match.");
-		}
-	};
-}
+				Assert::AreEqual(std::string("Temp:70,Rain:1,40,Thunder:1,Hail:0,Windy:1,20"), l, L"Data does not match.");
+			}
+		};
+	}
